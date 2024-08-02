d94d81190ec46ac2d0b7bfd2cf4f3164e7921e7d
Soham <71167911+soham-basak@users.noreply.github.com>
2024-08-03 03:03:20 +0530

    style: sidebar menu items

--- src/www/components/LeftMenu.tsx
+++ src/www/components/LeftMenu.tsx
@@ -12,18 +12,26 @@ const LeftMenu = (props: PropsWithChildren & JSX.IntrinsicElements['div']) => {
       </div>
       <div class='no-scrollbar drawer-side z-[999] mt-4'>
         <label for='my-drawer-2' aria-label='close sidebar' class='drawer-overlay'></label>
-        <div className='flex items-center justify-center gap-2'>
+        <div className='mb-4 ml-8 flex items-center justify-start gap-2'>
           <img src='/public/assets/logo-light.png' alt='Logo' className='w-12 rounded-lg' />
-          <h1 className='text-whiter text-4xl font-semibold'>Dashboard</h1>
+          <h1 className='text-whiter text-3xl font-semibold'>Dashboard</h1>
         </div>
-        <ul class='menu min-h-full w-80 bg-base-200 p-4 text-base-content'>
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
-        </ul>
+        <nav class='menu mt-5 min-h-full w-80 px-4 py-4 text-base-content lg:mt-9 lg:px-6'>
+          <div>
+            <ul class='mb-5.5 mt-4 flex flex-col gap-2.5 pl-6'>
+              <li>
+                <a class='text-bodydark2 group relative flex items-center gap-2.5 rounded-md px-4 font-medium duration-300 ease-in-out hover:text-white'>
+                  Posts
+                </a>
+              </li>
+              <li>
+                <a class='text-bodydark2 group relative flex items-center gap-2.5 rounded-md px-4 font-medium duration-300 ease-in-out hover:text-white'>
+                  Users
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     </div>
   );
